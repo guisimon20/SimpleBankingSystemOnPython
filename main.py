a231740fa7fcd9d3cde06171ee8f8433277ebe08
guisimon20 <guigvsimon20@gmail.com>
2024-06-03 21:15:44 +0000

    bugs fixes, new limit viewer and changer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import datetime
+from time import sleep
 
 saldo = 0
 limite = 500
@@ -8,6 +9,7 @@ LIMITES_SAQUES = 3
 contador_extrato = 0
 data_atual = datetime.datetime.now()
 data_formatada = data_atual.strftime("%d/%m/%Y")
+total_sacado = 0
 
 print("=" * 30)
 print("SISTEMA BANCÁRIO".center(30))
@@ -22,33 +24,56 @@ while True:
   [D] Depósito
   [S] Saque
   [E] Extrato
+  [L] Limites
   [Q] Sair
 
   =>"""
   opcao = input(menu)
-  
+
   if opcao.lower() == "d":
     print("Depósito".center(30, "-"))
     deposito = int(input("Quanto deseja depositar? "))
-    saldo += deposito
-    contador_extrato += 1
-    extrato += f"{contador_extrato} - Depósito de R${deposito} \n"
-    print(f"Seu depósito de R${deposito} foi concluído")
+    if deposito > 0:
+      saldo += deposito
+      contador_extrato += 1
+      extrato += f"{contador_extrato} - Depósito de R${deposito} \n"
+      print(f"Seu depósito de R${deposito} foi concluído")
+    else:
+      print("O valor digitado não é válido!")
+      print("Redirecionando para o menu...")
+      sleep(1)
   elif opcao.lower() == "s":
     print("Saque".center(30, "-"))
-    saque = int(input("Quanto deseja sacar? "))
-    if saque <= limite and numero_saques < LIMITES_SAQUES:
-      numero_saques += 1
-      saldo -= saque
-      contador_extrato += 1
-      extrato += f"{contador_extrato} - Saque de R${saque} \n"
-      print(f"Seu saque de R${saque} foi concluído")
-    elif saque <= limite and numero_saques >= LIMITES_SAQUES:
-      print("Foi excedido o número de saques diário!")
-    elif saque > limite and numero_saques < LIMITES_SAQUES:
-      print("Seu limite de saque foi excedido!")
+    saque = input("Quanto deseja sacar? ")
+    if saque.isnumeric() and int(saque) > 0:
+      saque = int(saque)
+      if saque <= limite and numero_saques < LIMITES_SAQUES and saque <= saldo:
+        numero_saques += 1
+        saldo -= saque
+        contador_extrato += 1
+        total_sacado += saque
+        extrato += f"{contador_extrato} - Saque de R${saque} \n"
+        print(f"Seu saque de R${saque} foi concluído")
+      elif saque <= limite and numero_saques >= LIMITES_SAQUES:
+        print("Foi excedido o número de 3 saques diário!")
+      elif saque > limite and numero_saques < LIMITES_SAQUES:
+        print("Seu limite de saque foi excedido!")
+      else:
+        print("Sem saldo suficiente para concluir operação!")
     else:
-      print("Seu limite de saque e número de saques diário foram excedidos!")
+      print("O valor digitado não é válido!")
+      print("Redirecionando para o menu...")
+      sleep(1)
+  elif opcao.lower() == "l":
+    print("Limites".center(30, "-"))
+    print(f"Você usou R${total_sacado}/{limite}")
+    print(f"Você sacou {numero_saques}/{LIMITES_SAQUES} vezes")
+    esc = input("Deseja alterar o limite de saque? [S/N] ")
+    if esc.lower() == "s":
+      limite = int(input("Qual será seu novo limite?"))
+    else:
+
+      sleep(1)
   elif opcao.lower() == "e":
     print("Extrato".center(30, "-"))
     print(extrato)
@@ -58,4 +83,3 @@ while True:
   else:
     print("Erro! Opção Inexistente, Tente Novamente!")
 print("Programa Finalizado, Muito Obrigado!")
-
